51aebdcca6a3b1b66b2145b2a8a6ca7197973c3f
Дмитрий <waiser-7@mail.ru>
2018-12-29 15:13:22 +0500

    vince: fix open apps junks

--- prop.mk
+++ prop.mk
@@ -109,6 +109,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	vendor.display.disable_skip_validate=1 \
 	vendor.display.enable_default_color_mode=1 \
 	vendor.gralloc.enable_fb_ubwc=1 \
+	vendor.display.disable_rotator_downscale=1 \
 	debug.sf.latch_unsignaled=1
 
 	
