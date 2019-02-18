89827482dac0134face11e7e793a7a7568c79499
wufixx <wideapr@gmail.com>
2019-02-18 20:36:01 +0700

    vince: Add camera packages

--- device.mk
+++ device.mk
@@ -125,9 +125,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    camera.device@3.2-impl \
+    camera.msm8953 \
+    libmm-qcamera \
+    Snap \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
-    Snap
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Consumerir
 PRODUCT_PACKAGES += \
