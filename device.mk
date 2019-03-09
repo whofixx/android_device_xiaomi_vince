15515cb0c38a2dae22731bdc67a430dc0b903a39
wufixx <wideapr@gmail.com>
2019-03-09 19:04:10 +0700

    vicne: Revert Build Touch HAL to enable KeyDisabler

--- device.mk
+++ device.mk
@@ -373,10 +373,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:system/lib64/android.hardware.gnss@1.0-v27.so
 
-# Touch HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi_8953
-
 # Thermal
 PRODUCT_PACKAGES += \
     thermal.msm8953
